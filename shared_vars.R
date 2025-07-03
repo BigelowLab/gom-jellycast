20b6cfc5785bca93093a609477cdb7e202ea4ecd
Logan Ngai <llngai26@colby.edu>
2025-07-03 12:40:57 -0400

    adding shared_vars.R

---
+++ shared_vars.R
@@ -0,0 +1,14 @@
+lionsmane_img = update_forecast(version = "v0", v = "v0.015", model = "rf")
+lionsmane_gif = make_movie(version = "v0", v = "v0.015", start_date = as.Date("2025-07-01"), end_date = as.Date("2025-07-08"), model = "rf")
+
+
+moon_img = update_forecast(version = "v0", v = "v0.021", model = "rf")
+moon_gif = make_movie(version = "v0", v = "v0.021", start_date = as.Date("2025-07-01"), end_date = as.Date("2025-07-08"), model = "rf")
+
+
+whitecross_img = update_forecast(version = "v0", v = "v0.028", model = "rf")
+whitecross_gif = make_movie(version = "v0", v = "v0.028", start_date = as.Date("2025-07-01"), end_date = as.Date("2025-07-08"), model = "rf")
+
+
+comb_img = update_forecast(version = "v0", v = "v0.033", model = "rf")
+comb_gif = make_movie(version = "v0", v = "v0.033", start_date = as.Date("2025-07-01"), end_date = as.Date("2025-07-08"), model = "rf")
