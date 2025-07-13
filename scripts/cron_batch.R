e173c845bb911f5ad725af41a1d7657876670a1f
Logan Ngai <llngai26@colby.edu>
2025-07-13 08:20:29 -0400

    2025-07-13T08:20:29 forecast update

--- scripts/cron_batch.R
+++ scripts/cron_batch.R
@@ -4,7 +4,7 @@ script1 = sprintf("batch_v01.R %s", format(Sys.Date() + 8, "%Y-%m-%d"))
 script2 = sprintf("batch_v11.R %s", format(Sys.Date() + 8, "%Y-%m-%d"))
 
 args1 = sprintf("%s/scripts/%s --configs %s/data/versions/v0/v0.015/v0.015.yaml %s/data/versions/v0/v0.021/v0.021.yaml %s/data/versions/v0/v0.028/v0.028.yaml %s/data/versions/v0/v0.033/v0.033.yaml", path, script1, path, path, path, path) 
-args2 = sprintf("%s/scripts/%s --configs %s/data/versions/v1/v1.015/v1.015.yaml %s/data/versions/v1/v1.021/v1.021.yaml %s/data/versions/v1/v1.028/v1.028.yaml %s/data/versions/v1/v1.033/v1.033.yaml", path, script2, path, path, path, path)
+args2 = sprintf("%s/scripts/%s --configs %s/data/versions/v1/v1.020/v1.020.yaml %s/data/versions/v1/v1.022/v1.022.yaml %s/data/versions/v1/v1.027/v1.027.yaml %s/data/versions/v1/v1.036/v1.036.yaml", path, script2, path, path, path, path)
 
 system2(app, args1)
 system2(app, args2)
--- shared_vars.R
+++ shared_vars.R
@@ -1,14 +1,14 @@
-lionsmane_img = extract_im(version = "v1", v = "v1.015", model = "rf")
-lionsmane_gif = make_movie(version = "v1", v = "v1.015", model = "rf")
+lionsmane_img = extract_im(version = "v1", v = "v1.020", model = "rf")
+lionsmane_gif = make_movie(version = "v1", v = "v1.020", model = "rf")
 
 
-moon_img = extract_im(version = "v1", v = "v1.021", model = "rf")
-moon_gif = make_movie(version = "v1", v = "v1.021", model = "rf")
+moon_img = extract_im(version = "v1", v = "v1.022", model = "rf")
+moon_gif = make_movie(version = "v1", v = "v1.022", model = "rf")
 
 
-whitecross_img = extract_im(version = "v1", v = "v1.028", model = "rf")
-whitecross_gif = make_movie(version = "v1", v = "v1.028", model = "rf")
+whitecross_img = extract_im(version = "v1", v = "v1.027", model = "rf")
+whitecross_gif = make_movie(version = "v1", v = "v1.027", model = "rf")
 
 
-comb_img = extract_im(version = "v1", v = "v1.033", model = "rf")
-comb_gif = make_movie(version = "v1", v = "v1.033", model = "rf")
+comb_img = extract_im(version = "v1", v = "v1.036", model = "rf")
+comb_gif = make_movie(version = "v1", v = "v1.036", model = "rf")
