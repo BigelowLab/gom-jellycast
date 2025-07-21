606b2e27b085bf752436c5a6b58c46ca7b2f4c65
Logan Ngai <llngai26@colby.edu>
2025-07-21 13:17:54 -0400

    2025-07-21T13:17:54 forecast update

--- scripts/cron_batch.R
+++ scripts/cron_batch.R
@@ -1,13 +1,23 @@
-path = "/mnt/s1/projects/ecocast/projects/gom-jellycast-dev"
-app = "Rscript"
-script1 = sprintf("batch_v02.R %s", format(Sys.Date() + 8, "%Y-%m-%d"))
-script2 = sprintf("batch_v12.R %s", format(Sys.Date() + 8, "%Y-%m-%d"))
+path <- "/mnt/s1/projects/ecocast/projects/gom-jellycast-dev"
+app <- "Rscript"
+script1 <- sprintf("batch_v02.R %s", format(Sys.Date() + 8, "%Y-%m-%d"))
+script2 <- sprintf("batch_v12.R %s", format(Sys.Date() + 8, "%Y-%m-%d"))
 
-args1 = sprintf("%s/scripts/%s --configs %s/data/versions/v0/v0.001/v0.001.yaml %s/data/versions/v0/v0.038/v0.038.yaml", path, script1, path, path)
+args1 = sprintf("%s/scripts/%s --configs %s/data/versions/v0/v0.015/v0.015.yaml %s/data/versions/v0/v0.038/v0.038.yaml", path, script1, path, path)
 args2 = sprintf("%s/scripts/%s --configs %s/data/versions/v1/v1.015/v1.015.yaml %s/data/versions/v1/v1.038/v1.038.yaml", path, script2, path, path)
 
-system2(app, args1)
-system2(app, args2)
+out1 <- system2(app, args1, stdout = TRUE, stderr = TRUE)
+charlier::sendmail(to = "llngai26@colby.edu", subject = "done running batch_v02.R")
+status1 <- attr(out1, "status")
+if (is.null(status1)) status1 <- 0
+if (status1 != 0) {
+  charlier::sendmail(to = "llngai26@colby.edu", subject = "error in batch_v02.R", message = out1)
+}
 
-charlier::sendmail(to = "llngai26@colby.edu", subject = "updating predictions")
-
+out2 <- system2(app, args2, stdout = TRUE, stderr = TRUE)
+charlier::sendmail(to = "llngai26@colby.edu", subject = "done running batch_v12.R")
+status2 <- attr(out2, "status")
+if (is.null(status2)) status2 <- 0
+if (status2 != 0) {
+  charlier::sendmail(to = "llngai26@colby.edu", subject = "error in batch_v12.R", message = out2)
+}
